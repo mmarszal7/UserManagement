7715f4906d514066e23213ed341102e5891c1686
mmarszal7 <mmarszal7@gmail.com>
2018-09-26 22:35:39 +0200

    Group name edit form

--- UserManagement_Angular/ClientApp/src/app/app.module.ts
+++ UserManagement_Angular/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -41,6 +41,7 @@ const appRoutes: Routes = [
         BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
         HttpClientModule,
         FormsModule,
+        ReactiveFormsModule,
         Ng2TableModule,
         PaginationModule.forRoot(),
         RouterModule.forRoot(appRoutes)
--- UserManagement_Angular/ClientApp/src/app/components/groups/group-profile/group-profile.component.ts
+++ UserManagement_Angular/ClientApp/src/app/components/groups/group-profile/group-profile.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Group } from '../../../shared/models/group';
 import { ActivatedRoute } from '@angular/router';
 import { GroupsService } from '../../../shared/services/groups.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-group-profile',
@@ -11,12 +12,21 @@ export class GroupProfileComponent {
 
     group: Group;
 
+    form: FormGroup = new FormGroup({
+        name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    });
+
     constructor(private dataService: GroupsService, private route: ActivatedRoute) {
         const id = this.route.snapshot.params['id'];
-        this.dataService.getGroup(id).subscribe(g => this.group = g);
+        this.dataService.getGroup(id).subscribe(g => {
+            this.group = g;
+            this.form.patchValue({ name: g.name });
+        });
     }
 
-    editName() {
-        console.log(this.group.name);
+    submit() {
+        if (this.form.valid) {
+            console.log(this.form.controls['name']);
+        }
     }
 }
